c11a603012009a93c0d17219a4b50bed4462859a
rahil ghamraci <ghamracirahil@gmail.com>
2024-04-29 22:37:06 +0200

    Added new files of the second part

---
+++ src/projet/tech/ag/SellerAgent.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projet.tech.ag;
+import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
+import java.util.Random;
+
+/**
+ *
+ * @author Dell
+ */
+public class SellerAgent extends Agent{
+       
+    protected void setup() {
+        addBehaviour(new OfferRequestsServer());
+    }
+
+    private class OfferRequestsServer extends CyclicBehaviour {
+        public void action() {
+            ACLMessage msg = myAgent.receive();
+            if (msg != null) {
+                ACLMessage reply = msg.createReply();
+                reply.setPerformative(ACLMessage.PROPOSE);
+                // Ici vous pouvez définir la proposition d'achat en fonction de l'offre du vendeur
+                // Par exemple, vous pouvez inclure les détails sur le produit et son prix.
+                 int price = generateRandomValue(5000, 10000);
+                 int quality = generateRandomValue(1, 5);
+                 int deliveryCost = generateRandomValue(5, 15);
+               
+                 String content = "Prix: " + price + ", Qualité: " + quality + ", Frais de livraison: " + deliveryCost;
+                 System.out.println(content+" seller:"+ getLocalName());
+                reply.setContent(content);
+                myAgent.send(reply);
+            } else {
+                block();
+            }
+        }
+           private int generateRandomValue(int min, int max) {
+            Random random = new Random();
+            return random.nextInt(max - min + 1) + min;
+        }
+    }
+    
+}
